2dcaab3aa296949270d39a0d6e63ebaa07745d73
neo050 <neoray.asking666@gmail.com>
2022-06-12 22:35:21 +0300

    22:35 12/06/22

---
+++ ex2pro/src/IntLinkedList.java
@@ -0,0 +1,122 @@
+public class IntLinkedList {
+
+    private class Node {
+        private int val;
+        private Node next;
+
+        private Node(int val) {
+            this.val = val;
+            next = null;
+        }
+
+        private Node(int val, Node nextp) {
+            this.val = val;
+            next = nextp;
+        }
+
+        private int getval() {
+            return val;
+        }
+
+        private boolean hasnext() {
+            return next != null;
+        }
+
+        public Node getnext() {
+            return next;
+        }
+
+
+    }
+
+    private Node head;
+
+    public Node find(int val) {
+        Node temp = head;
+        if (temp.getval() == val)
+            return temp;
+        while (temp.hasnext()) {
+            if (temp.getval() == val)
+                return temp;
+            else {
+                temp = temp.getnext();
+            }
+
+        }
+
+        return null;
+    }
+
+    public boolean remove(Node remove) {
+        if (remove == head) {
+            head = head.getnext();
+            return true;
+        }
+        if (find(remove.getval()) != null) ;
+        {
+            Node temp = head;
+            while (temp.hasnext()) {
+
+                if (temp.getnext() == remove) {
+                    temp = temp.getnext().getnext();
+                    return true;
+                } else {
+                    temp = temp.getnext();
+                }
+
+            }
+
+        }
+        return false;
+    }
+
+    public boolean add(int val)
+    {
+        Node add=new Node(val);
+        if (find(add.getval()) == null) {
+            add.next = head.getnext();
+            head.next = add;
+            return true;
+        }
+        return false;
+    }
+
+    public IntLinkedList(int val) {
+        head = new Node(val);
+    }
+
+    public IntLinkedList(int val, Node nextp) {
+        head = new Node(val, nextp);
+    }
+
+    public void print() {
+        Node temp = head;
+        System.out.println("");
+        while (temp!=null) {
+            if(temp.getnext()!=null)
+            {
+                    System.out.print(temp.getval()+"-->");
+            }
+            else
+                System.out.print(temp.getval());
+
+            temp=temp.getnext();
+
+        }
+    }
+
+    public static void main(String [] args)
+    {
+        int i =0;
+        IntLinkedList myIntLinkedList=new IntLinkedList(1);
+        while (i<1000)
+        {
+            myIntLinkedList.add(i);
+            i++;
+        }
+
+
+        myIntLinkedList.print();
+
+    }
+}
